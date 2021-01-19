6c80210132e0b3abf175243f8c3d7e390540b74f
James Jensen <jnjensen@live.com>
2021-01-19 03:45:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function iterativeLog(array){
   array.forEach(console.log("${index} : ${element}"))
 }
+
+function iterate(callback){
+  var testing = ["blue", "green", "purple", "orange"]
+  
+  
+}
